f0c21f7a10f1275ed8065e9fa76962a692860bfe
Esteban Salazar <esteban.salazar@pragma.com.co>
2018-01-30 08:09:28 -0500

    Nuevo avance, si deja sin terminar

--- src/main/java/com/boamorte/beans/Mundo.java
+++ src/main/java/com/boamorte/beans/Mundo.java
@@ -1,7 +1,10 @@
 package com.boamorte.beans;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Mundo {
 
+	@Value("Hola Mundo! con annot...")
 	private String saludo;
 
 	public String getSaludo() {
--- src/main/java/com/boamorte/springmitocode/App.java
+++ src/main/java/com/boamorte/springmitocode/App.java
@@ -2,15 +2,18 @@ package com.boamorte.springmitocode;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.boamorte.beans.AppConfig;
 import com.boamorte.beans.Mundo;
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/boamorte/xml/beans.xml");
+//		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/boamorte/xml/beans.xml");
+		ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
 		Mundo m = appContext.getBean(Mundo.class); // Tambien podria ser = (Mundo) appContext.getBean("mundo");
 		
 		System.out.println(m.getSaludo());
